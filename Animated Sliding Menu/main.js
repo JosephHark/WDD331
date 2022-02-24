85b9f28909d5a1f1ac91c87bec59663d1bbe0e3f
jhark <jhark478@gmail.com>
2022-02-24 16:27:27 -0500

    c

--- Animated Sliding Menu/main.js
+++ Animated Sliding Menu/main.js
@@ -1,5 +1,13 @@
 function toggleNav() {
     var updateElement = document.getElementById("menu-icon");
- //toggle adds a class if it's not there or removes it if it is.
- updateElement.classList.toggle("open");
-}              
+    //toggle adds a class if it's not there or removes it if it is.
+    updateElement.classList.toggle("open")
+
+    var updateElement = document.getElementById("sidebar");
+    //toggle adds a class if it's not there or removes it if it is.
+    updateElement.classList.toggle("sidebar");
+
+    var updateElement = document.getElementById("under");
+    //toggle adds a class if it's not there or removes it if it is.
+    updateElement.classList.toggle("under");
+}
