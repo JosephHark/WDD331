918a614890807ce00f10b9080b93ab23d9f3879d
jhark <jhark478@gmail.com>
2022-03-20 15:03:31 -0400

    a

--- basic/js/merc.js
+++ basic/js/merc.js
@@ -1,10 +1,20 @@
-function createfn(){
-    //debugger;
-    var element = document.createElement("div");
-    var para = document.createTextNode('The man who mistook his wife for a hat');
-     para.className = "mystyle";
-    element.appendChild(para);
-    document.getElementsByTagName('body')[0].appendChild(element);
+const render = () => {
+    const element = document.createElement('div');
+    element.innerHTML = `
+    <div class="milkyWay"> 
+    <div class="sun"></div> 
+    <div class="mercury"> <img src="./images/mercury.png" alt="mercury"> </div>
+    <div class="venus"><img src="./images/Venus.png" alt="venus"></div>
+    <div class="earth"><img src="./images/earth.png" alt="earth"></div>
+    <divclass="mars"><img src="./images/mars.png" alt="mars"></div>
+    <div class="jupiter"><img src="./images/jupiter.png" alt="jupiter"></div>
+    <div class="saturn"><img src="./images/saturn.png" alt="saturn"></div>
+    <div class="uranus"><img src="./images/uranus.png" alt="uranus"></div>
+    <div class="neptune"><img src="./images/Neptune.png" alt="Neptune"></div>
+    </div>`
 
-    }
-    window.onload=createfn();
+    // I'll just add the above to the end body tag, but you can append anywhere
+    document.body.append(element)
+}
+
+window.onload = render()
